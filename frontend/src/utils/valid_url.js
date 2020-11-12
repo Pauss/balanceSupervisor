59035181d2d54736b930e7546e233fde3693d5ab
Pau <tpr.paus@gmail.com>
2020-11-13 00:22:15 +0200

    updated valid_urls for production

--- frontend/src/utils/valid_url.js
+++ frontend/src/utils/valid_url.js
@@ -4,15 +4,17 @@ const IP_DEV = '192.168.100.15'
 const PORT_DEV = '4000'
 const PORT_PROD = '80'
 
-const IP = process.env.NODE_ENV === 'production' ? IP_PROD : IP_DEV
-const PORT = process.env.NODE_ENV === 'production' ? '' : PORT_DEV
+const ADDRESS_PROD = 'http://cippau.go.ro:' + PORT_PROD + 'balance-supervisor/api'
+const ADDRESS_DEV = 'http://' + IP_DEV + ':' + PORT_DEV
+
+const ADDRESS = process.env.NODE_ENV === 'production' ? ADDRESS_PROD : ADDRESS_DEV
 
 const URLs = {
-  login: `http://${IP}:${PORT}/api/auth`,
-  register: `http://${IP}:${PORT}/api/users/register`,
-  logCost: `http://${IP}:${PORT}/api/costs/log`,
-  getDashboard: `http://${IP}:${PORT}/api/costs/currentLogs`,
-  history: `http://${IP}:${PORT}/api/costs/history`
+  login: `${ADDRESS}/api/auth`,
+  register: `${ADDRESS}/api/users/register`,
+  logCost: `${ADDRESS}/api/costs/log`,
+  getDashboard: `${ADDRESS}/api/costs/currentLogs`,
+  history: `${ADDRESS}/api/costs/history`
 }
 
 export default URLs
