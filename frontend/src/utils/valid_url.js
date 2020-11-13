d73131bd23b62d6919a40315c8789ba329c91f02
Ciprian Andrei <ciprian.andrei@digitalya.co>
2020-11-13 19:07:44 +0200

    added https to production

--- frontend/src/utils/valid_url.js
+++ frontend/src/utils/valid_url.js
@@ -1,21 +1,21 @@
-const IP_PROD = 'cippau.go.ro/balance-supervisor/api'
-const IP_DEV = 'localhost'
+const IP_PROD = 'cippau.go.ro/balance-supervisor/api';
+const IP_DEV = 'localhost';
 //'192.168.100.15'
 
-const PORT_DEV = '4000'
-const PORT_PROD = '80'
+const PORT_DEV = '4000';
+const PORT_PROD = '80';
 
-const ADDRESS_PROD = 'http://cippau.go.ro:' + PORT_PROD + '/balance-supervisor/api'
-const ADDRESS_DEV = 'http://' + IP_DEV + ':' + PORT_DEV
+const ADDRESS_PROD = 'https://cippau.go.ro:' + PORT_PROD + '/balance-supervisor/api';
+const ADDRESS_DEV = 'http://' + IP_DEV + ':' + PORT_DEV;
 
-const ADDRESS = process.env.NODE_ENV === 'production' ? ADDRESS_PROD : ADDRESS_DEV
+const ADDRESS = process.env.NODE_ENV === 'production' ? ADDRESS_PROD : ADDRESS_DEV;
 
 const URLs = {
   login: `${ADDRESS}/api/auth`,
   register: `${ADDRESS}/api/users/register`,
   logCost: `${ADDRESS}/api/costs/log`,
   getDashboard: `${ADDRESS}/api/costs/currentLogs`,
-  history: `${ADDRESS}/api/costs/history`,
-}
+  history: `${ADDRESS}/api/costs/history`
+};
 
-export default URLs
+export default URLs;
