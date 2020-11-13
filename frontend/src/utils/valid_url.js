4271035682e611c1d8f69bf6373a41efa00b0e37
Ciprian Andrei <ciprian.andrei@digitalya.co>
2020-11-13 20:19:57 +0200

    fixed api url

--- frontend/src/utils/valid_url.js
+++ frontend/src/utils/valid_url.js
@@ -2,11 +2,8 @@ const IP_PROD = 'cippau.go.ro/balance-supervisor/api';
 const IP_DEV = 'localhost';
 //'192.168.100.15'
 
-const PORT_DEV = '4000';
-const PORT_PROD = '80';
-
-const ADDRESS_PROD = 'https://cippau.go.ro:' + PORT_PROD + '/balance-supervisor/api';
-const ADDRESS_DEV = 'http://' + IP_DEV + ':' + PORT_DEV;
+const ADDRESS_PROD = 'https://cippau.go.ro/balance-supervisor/api';
+const ADDRESS_DEV = 'http://127.0.0.1:4000';
 
 const ADDRESS = process.env.NODE_ENV === 'production' ? ADDRESS_PROD : ADDRESS_DEV;
 
